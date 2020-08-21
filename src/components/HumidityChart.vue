aa99369337441ac076274cdbee85efbdeec26b63
Felipe Cardozo <dafelcardozo@gmail.com>
2020-08-21 18:33:52 -0500

    More data plotted.

---
+++ src/components/HumidityChart.vue
@@ -0,0 +1,36 @@
+
+<script>
+import { Line, mixins  } from 'vue-chartjs';
+const { reactiveProp } = mixins
+
+export default {
+  extends: Line,
+  mixins: [reactiveProp],
+  props: {
+    chartData: {
+      type: Object,
+      default: null
+    },
+    options: {
+      type: Object,
+      default: null
+    }
+  },
+  mounted () {
+    this.renderChart(this.chartData,  {
+      elements: {
+        line: {
+          borderColor: "rgb(255,0,0)"
+        }
+      },
+
+    }
+  )
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- src/components/MonthlyChartsContainer.vue
+++ src/components/MonthlyChartsContainer.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container">
     <div>Month: {{month}} {{year}}</div>
-    <pressure-line v-if="loaded"
-                   :chartData="pressure"
-                   />
+    <pressure-line v-if="loaded" :chartData="pressure" />
+    <temperature-line v-if="loaded" :chartData="temperature"/>
+    <humidity-chart v-if="loaded" :chartData="humidity" />
   </div>
 </template>
 
@@ -11,12 +11,16 @@
 
 import axios from 'axios';
 import PressureLine from "@/components/PressureLine";
+import TemperatureLine from "@/components/TemperatureLine";
+import HumidityChart from "@/components/HumidityChart";
 
 export default {
-  components: {PressureLine  },
+  components: {HumidityChart, TemperatureLine, PressureLine  },
   data: () => ({
     loaded: false,
-    pressure: null
+    pressure: null,
+    temperature:null,
+    humidity:null
   }),
   props: ['month', 'year'],
   watch: {
@@ -43,6 +47,31 @@ export default {
             label: '9 am Pressure this month',
             backgroundColor: "rgba(255,10,13, 0.2)",
             data: data.map(({air_pressure_9am}) => air_pressure_9am),
+          }
+        ]
+      };
+      this.temperature = {
+        labels: data.map(({date}) => date.split('-')[2]),
+        datasets: [
+          {
+            label: '9 am Temperature this month',
+            backgroundColor: "rgba(10,10,255, 0.2)",
+            data: data.map(({air_temp_9am}) => air_temp_9am),
+          }
+        ]
+      };
+      this.humidity = {
+        labels: data.map(({date}) => date.split('-')[2]),
+        datasets: [
+          {
+            label: '9 am Relative humidity this month',
+            backgroundColor: "rgba(10,255,0, 0.2)",
+            data: data.map(({relative_humidity_9am}) => relative_humidity_9am),
+          },
+          {
+            label: '9 am Relative humidity this month',
+            backgroundColor: "rgba(240,230,140, 0.9)",
+            data: data.map(({relative_humidity_3pm}) => relative_humidity_3pm),
           }
         ]
       }
