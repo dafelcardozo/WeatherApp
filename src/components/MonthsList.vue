ded97557c89b5f59e8d48bf15812a7ea49999254
Felipe Cardozo <dafelcardozo@gmail.com>
2020-08-31 17:30:44 -0500

    Removes the scroll from the period list, by splitting it in two.

--- src/components/MonthsList.vue
+++ src/components/MonthsList.vue
@@ -1,12 +1,29 @@
 <template>
-  <div>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <b-list-group>
+          <b-list-group-item button v-for="year in years" :key="year" v-on:click="() => setActiveYear(year)" :active="year === activeYear">
+            {{year}}
+          </b-list-group-item>
+        </b-list-group>
+      </div>
+      <div class="col">
+        <b-list-group>
+          <b-list-group-item button v-for="month in monthNames" :key="month" v-on:click="() => setActiveMonth(month)" :active="month === activeMonth">
+            {{month}}
+          </b-list-group-item>
+        </b-list-group>
+      </div>
+    </div>
+    <!--
     <b-list-group>
       <b-list-group-item button v-for="[year, month, str] in months" :key="str" v-on:click="() => setMonth(year, month, str)"
                          :active="activeMonth === month && activeYear === year">
         {{str}}
       </b-list-group-item>
     </b-list-group>
-
+-->
   </div>
 </template>
 
@@ -19,19 +36,30 @@ export default {
   data: () => ({
     months:[],
     activeMonth:'',
-    activeYear:''
+    activeYear:'',
+    years: [],
+    monthNames: []
   }),
   async mounted () {
       const {data} = await axios.get(`${process.env.VUE_APP_WS_URL}/available_months`);
       this.months = data;
+      this.years = Array.from(new Set(this.months.map(([year,]) => year)));
+      this.monthNames = [...Array(12).keys()].map(i => new Date(2020, i, 1).toLocaleString(navigator.language, { month: "long" }));
   },
   methods: {
-    setMonth( year, month, dateRangeStr) {
-      console.info('click handler');
-      this.$emit('switchedDateRange',{month, year, dateRangeStr});
-      this.activeMonth = month;
+    setActiveYear(year) {
       this.activeYear = year;
+      this.callUpdate();
+    },
+    setActiveMonth(month) {
+      this.activeMonth = month;
+      this.callUpdate();
+    },
+    callUpdate() {
+      if (this.activeMonth && this.activeYear)
+        this.$emit('switchedDateRange',{month: this.monthNames.indexOf(this.activeMonth), year:this.activeYear, dateRangeStr:this.activeMonth+", "+this.activeYear});
     }
+
   }
 }
 </script>
